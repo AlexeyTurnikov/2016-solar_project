b6611d1f2e7ca49865b3a4e5097a08e8a3ce4dd3
alexe <tiurnikov.aiu@phystech.edu>
2021-11-10 00:09:35 +0300

    В процессе Планеты улетают, есть предположение, что перепутаны знаки у сил. Не соблюдено полностью PEPE8

--- solar_main.py
+++ solar_main.py
@@ -1,11 +1,11 @@
 # coding: utf-8
 # license: GPLv3
 
-import tkinter
+import pygame
 from tkinter.filedialog import *
-from solar_vis import *
-from solar_model import *
-from solar_input import *
+import solar_vis as vis
+import solar_model as model
+import solar_input
 
 perform_execution = False
 """Флаг цикличности выполнения расчёта"""
@@ -24,72 +24,73 @@ time_step = None
 
 space_objects = []
 """Список космических объектов."""
+HEIGHT = 800
+WIDTH = 600
+FPS = 30
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+GREY = (128, 128, 128)
+RED = (200, 50, 100)
+WHITE = (255, 255, 255)
 
 
-def execution():
-    """Функция исполнения -- выполняется циклически, вызывая обработку всех небесных тел,
-    а также обновляя их положение на экране.
-    Цикличность выполнения зависит от значения глобальной переменной perform_execution.
-    При perform_execution == True функция запрашивает вызов самой себя по таймеру через от 1 мс до 100 мс.
-    """
-    global physical_time
-    global displayed_time
-    recalculate_space_objects_positions(space_objects, time_step.get())
-    for body in space_objects:
-        update_object_position(space, body)
-    physical_time += time_step.get()
-    displayed_time.set("%.1f" % physical_time + " seconds gone")
-
-    if perform_execution:
-        space.after(101 - int(time_speed.get()), execution)
+def writing(text: str, xcenter, ycenter, font_size=16):
+    font = pygame.font.SysFont('Arial', font_size)
+    words = font.render(text, True, (0, 0, 0))
+    place = words.get_rect(center=(xcenter, ycenter))
+    SCREEN.blit(words, place)
 
 
-def start_execution():
-    """Обработчик события нажатия на кнопку Start.
-    Запускает циклическое исполнение функции execution.
-    """
-    global perform_execution
-    perform_execution = True
-    start_button['text'] = "Pause"
-    start_button['command'] = stop_execution
+class Button:
 
-    execution()
-    print('Started execution...')
+    def __init__(self, x, y, length, width, text: str, ):
+        self.x = x
+        self.y = y
+        self.length = length
+        self.width = width
+        self.text = text
+        self.color = GREY
+        self.pressed = FALSE
+        self.timer = 0
 
-
-def stop_execution():
-    """Обработчик события нажатия на кнопку Start.
-    Останавливает циклическое исполнение функции execution.
-    """
-    global perform_execution
-    perform_execution = False
-    start_button['text'] = "Start"
-    start_button['command'] = start_execution
-    print('Paused execution.')
-
-
-def open_file_dialog():
-    """Открывает диалоговое окно выбора имени файла и вызывает
-    функцию считывания параметров системы небесных тел из данного файла.
-    Считанные объекты сохраняются в глобальный список space_objects
-    """
-    global space_objects
-    global perform_execution
-    perform_execution = False
-    for obj in space_objects:
-        space.delete(obj.image)  # удаление старых изображений планет
-    in_filename = askopenfilename(filetypes=(("Text file", ".txt"),))
-    space_objects = read_space_objects_data_from_file(in_filename)
-    max_distance = max([max(abs(obj.x), abs(obj.y)) for obj in space_objects])
-    calculate_scale_factor(max_distance)
-
-    for obj in space_objects:
-        if obj.type == 'star':
-            create_star_image(space, obj)
-        elif obj.type == 'planet':
-            create_planet_image(space, obj)
+    def draw(self):
+        if self.pressed is TRUE:
+            self.color = RED
         else:
-            raise AssertionError()
+            self.color = GREY
+        pygame.draw.rect(SCREEN, self.color, (self.x, self.y, self.length, self.width))
+        if self.timer > 0:
+            self.timer += 1
+        if self.timer % 30 == 0:
+            self.pressed = False
+        writing(self.text, self.x + self.length / 2, self.y + self.width / 2)
+
+    def is_button_pressed(self, event):
+        if self.x < event.pos[0] < self.x + self.length and self.y < event.pos[1] < self.y + self.width:
+            self.pressed = TRUE
+            self.timer = 1
+            print("pressed")
+
+
+class Timer(Button):
+    def __init__(self, x, y, length, width, text: str):
+        super().__init__(x, y, length, width, text)
+        self.time = "1"
+
+    def draw(self):
+        super().draw()
+        writing(self.time, self.x + self.length / 2, self.y + 2 * self.width / 3)
+
+    def update(self, event):
+        if event.key == pygame.K_BACKSPACE and len(self.time) >= 1:
+            self.time = self.time[:-1]
+        elif len(self.time) <= 10:
+            self.time += event.unicode
+            self.time = "".join(c for c in self.time if c.isdecimal())  # проверяет введена ли цифра
+
+    def set_physical_time(self):
+        time_step = int(self.time)
+        return time_step
 
 
 def save_file_dialog():
@@ -98,55 +99,60 @@ def save_file_dialog():
     Считанные объекты сохраняются в глобальный список space_objects
     """
     out_filename = asksaveasfilename(filetypes=(("Text file", ".txt"),))
-    write_space_objects_data_to_file(out_filename, space_objects)
+    solar_input.write_space_objects_data_to_file(out_filename, space_objects)
 
 
 def main():
     """Главная функция главного модуля.
     Создаёт объекты графического дизайна библиотеки tkinter: окно, холст, фрейм с кнопками, кнопки.
     """
-    global physical_time
-    global displayed_time
-    global time_step
-    global time_speed
-    global space
-    global start_button
-
+    pygame.init()
     print('Modelling started!')
     physical_time = 0
+    start = 1
+    distances =[]
+    finished = False
 
-    root = tkinter.Tk()
-    # космическое пространство отображается на холсте типа Canvas
-    space = tkinter.Canvas(root, width=window_width, height=window_height, bg="black")
-    space.pack(side=tkinter.TOP)
-    # нижняя панель с кнопками
-    frame = tkinter.Frame(root)
-    frame.pack(side=tkinter.BOTTOM)
+    start_button = Button(0, 600, 100, 200, "start")
+    timer = Timer(100, 600, 200, 200, "time:")
+    load_from_file_button = Button(300, 600, 150, 200, "load_from_file")
+    save_file_button = Button(450, 600, 150, 200, "save_file")
+    buttons = [start_button, load_from_file_button, save_file_button]
 
-    start_button = tkinter.Button(frame, text="Start", command=start_execution, width=6)
-    start_button.pack(side=tkinter.LEFT)
+    objects = solar_input.read_space_objects_data_from_file("solar_system.txt")
+    for object in objects:
+        distances.append(max(abs(object.get_x()), abs(object.get_y())))
+    max_distance = max(distances)
 
-    time_step = tkinter.DoubleVar()
-    time_step.set(1)
-    time_step_entry = tkinter.Entry(frame, textvariable=time_step)
-    time_step_entry.pack(side=tkinter.LEFT)
 
-    time_speed = tkinter.DoubleVar()
-    scale = tkinter.Scale(frame, variable=time_speed, orient=tkinter.HORIZONTAL)
-    scale.pack(side=tkinter.LEFT)
+    while not finished:
+        clock.tick(FPS)
+        SCREEN.fill(WHITE)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                finished = True
+            if event.type == pygame.MOUSEBUTTONUP:
+                for button in buttons:
+                    button.is_button_pressed(event)
+                    if start_button.pressed is True:
+                        start = 1
+            if event.type == pygame.KEYDOWN:
+                timer.update(event)
+        for button in buttons:
+            button.draw()
 
-    load_file_button = tkinter.Button(frame, text="Open file...", command=open_file_dialog)
-    load_file_button.pack(side=tkinter.LEFT)
-    save_file_button = tkinter.Button(frame, text="Save to file...", command=save_file_dialog)
-    save_file_button.pack(side=tkinter.LEFT)
+        if start == 1:
 
-    displayed_time = tkinter.StringVar()
-    displayed_time.set(str(physical_time) + " seconds gone")
-    time_label = tkinter.Label(frame, textvariable=displayed_time, width=30)
-    time_label.pack(side=tkinter.RIGHT)
+            for object in objects:
+                model.recalculate_space_objects_positions(objects, timer.set_physical_time())
+                vis.update_object_position(SCREEN, object, max_distance)
+                vis.image(SCREEN, object, max_distance)
 
-    root.mainloop()
+        timer.draw()
+        pygame.display.update()
+
     print('Modelling finished!')
+
 
 if __name__ == "__main__":
     main()
